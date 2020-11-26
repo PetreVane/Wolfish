e3a0716d98a4c26dfbee845326b9e2c0ba7b3e1e
Petre Vane <petre.vane@gmail.com>
2020-11-26 20:07:33 +0100

    Designed custom list cell

--- Wolfish/Models/Json/Meal.swift
+++ Wolfish/Models/Json/Meal.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+
+struct Meal: Decodable {
+    
+    let request: [MealDescription]
+}
+
+// MARK: - Request
+struct MealDescription: Decodable, Identifiable {
+    let requestDescription: String
+    let imageURL: String
+    let id: Int
+    let price: Double
+    let carbs, protein, calories: Int
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case requestDescription = "description"
+        case imageURL, id, price, carbs, protein, calories, name
+    }
+}
+
+
+struct MockData {
+    
+    static let sampleMeal = MealDescription(
+        requestDescription: "Test description",
+        imageURL: "Image URL",
+        id: 75,
+        price: 25.5,
+        carbs: 75,
+        protein: 50,
+        calories: 250,
+        name: "Yummy pancakes")
+    
+    static let meals = [sampleMeal, sampleMeal, sampleMeal, sampleMeal, sampleMeal, sampleMeal]
+}
--- Wolfish/Views/WListView.swift
+++ Wolfish/Views/WListView.swift
@@ -14,10 +14,32 @@ struct WListView: View {
     
     var body: some View {
         NavigationView {
-            Text("List View")
-                .navigationTitle("Snacks")
+            List(MockData.meals) { meal in
+                ListCell(meal: meal)
+            }.navigationTitle("Snacks")
         }
         
+    }
+}
+
+struct ListCell: View {
+    
+    var meal: MealDescription
+    
+    var body: some View {
+        HStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .frame(width: 125, height: 100, alignment: .center)
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading) {
+                Text(meal.name).font(.title2)
+                Text("£ \(meal.price, specifier: "%.1f")")
+                    .foregroundColor(.secondary).fontWeight(.semibold)
+            }.padding(.leading)
+        }
     }
 }
 
