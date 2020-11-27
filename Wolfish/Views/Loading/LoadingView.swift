12216dc6fe896324506e13f51004b3025175d2c8
Petre Vane <petre.vane@gmail.com>
2020-11-27 17:16:54 +0100

    added loading view

---
+++ Wolfish/Views/Loading/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Wolfish
+//
+//  Created by Petre Vane on 27/11/2020.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
