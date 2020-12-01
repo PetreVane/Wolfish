0cbc09ebd1bcbe145ea166c9609732a950d0e4fa
Petre Vane <petre.vane@gmail.com>
2020-12-01 11:15:50 +0100

    Image loader fetches a remote image or shows a placeholder when network fails

--- Wolfish/Utilities/ImageLoader.swift
+++ Wolfish/Utilities/ImageLoader.swift
@@ -5,4 +5,43 @@
 //  Created by Petre Vane on 30/11/2020.
 //
 
-import Foundation
+import SwiftUI
+
+
+class ImageLoader: ObservableObject {
+    
+    @Published var image: Image? = nil
+    
+    func loadImageFromURL(stringURL: String) {
+        NetworkManager.sharedInstance.fetchImageFrom(stringURL: stringURL) { [self] (UIImage) in
+            guard let image = UIImage else { return }
+            DispatchQueue.main.async { self.image = Image(uiImage: image) }
+        }
+    }
+    
+}
+
+
+struct RemoteImage: View {
+    
+    let image: Image?
+    
+    var body: some View {
+        image?.resizable() ?? Images.placeholder.resizable()
+    }
+}
+
+
+struct RemoteImageLoader: View {
+    
+    @StateObject var loader = ImageLoader()
+    let imageURL: String
+    
+    var body: some View {
+        RemoteImage(image: loader.image)
+            .onAppear(perform: {
+                loader.loadImageFromURL(stringURL: imageURL)
+            }
+        )
+    }
+}
