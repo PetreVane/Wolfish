d63f2c897b9bf1504091efb9faf67375cc8e5079
Petre Vane <petre.vane@gmail.com>
2020-12-08 11:08:41 +0100

    Added form and sections to AccountView

--- Wolfish/Views/AccountView/WAccountView.swift
+++ Wolfish/Views/AccountView/WAccountView.swift
@@ -10,11 +10,56 @@ import SwiftUI
 struct WAccountView: View {
     
     init() { customNavigationBar() }
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var emailAddress = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefill = false
+
+    
     
     var body: some View {
         NavigationView {
-            Text("Account View!")
-                .navigationTitle("Account")
+            Form {
+                Section(header: Text("Personal info")) {
+                    
+                    TextField("First name", text: $firstName)
+                        .disableAutocorrection(true)
+                    
+                    TextField("Last name", text: $lastName)
+                        .disableAutocorrection(true)
+                    
+                    TextField("Email address", text: $emailAddress)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    
+                    DatePicker("Birthdate", selection: $birthDate, displayedComponents: .date)
+            
+                }
+                
+                Section(header: Text("Additional info")) {
+                    Toggle("Extra napkins", isOn: $extraNapkins)
+                    Toggle("Frequent refill", isOn: $frequentRefill)
+                } .toggleStyle(SwitchToggleStyle(tint: Colors.primary))
+                
+                Section() {
+                    Button(action: {
+                        print("Values saved")
+                    }, label: {
+                        
+                        HStack(alignment: .center) {
+                            Spacer()
+                            Text("Save changes")
+                            Spacer()
+                        }
+                    })
+                }
+                
+                    
+            }.navigationTitle("Account")
+            .foregroundColor(Colors.primary)
         }
     }
 }
@@ -22,5 +67,6 @@ struct WAccountView: View {
 struct WAccountView_Previews: PreviewProvider {
     static var previews: some View {
         WAccountView()
+            .preferredColorScheme(.light)
     }
 }
