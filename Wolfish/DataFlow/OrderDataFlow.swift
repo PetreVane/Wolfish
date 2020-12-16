27213166c9a11055c4bfec955f58a5bda2a54cf0
Petre Vane <petre.vane@gmail.com>
2020-12-16 12:56:24 +0100

    Added Environment object - renamed file

---
+++ Wolfish/DataFlow/OrderDataFlow.swift
@@ -0,0 +1,8 @@
+//
+//  OrderDataFlow.swift
+//  Wolfish
+//
+//  Created by Petre Vane on 16/12/2020.
+//
+
+import Foundation
--- Wolfish/Views/Buttons/WButtonDesign.swift
+++ Wolfish/Views/Buttons/WButtonDesign.swift
@@ -1,5 +1,5 @@
 //
-//  WOrderButton.swift
+//  WButtonDesign.swift
 //  Wolfish
 //
 //  Created by Petre Vane on 07/12/2020.
@@ -7,15 +7,10 @@
 
 import SwiftUI
 
-struct WOrderButton: View {
+struct WButtonDesign: View {
     var price: LocalizedStringKey
    
-    
     var body: some View {
-        Button(action: {
-            print("Button pressed")
-            
-        }, label: {
             Text(price)
                 .fontWeight(.semibold)
                 .frame(width: 300, height: 50, alignment: .center)
@@ -23,13 +18,12 @@ struct WOrderButton: View {
                 .foregroundColor(.white)
                 .cornerRadius(15)
                 .shadow(radius: 10)
-        })
     }
 }
 
 struct WOrderButton_Previews: PreviewProvider {
     static var previews: some View {
-        WOrderButton(price: "Testing")
+        WButtonDesign(price: "Testing")
     }
 }
 
